df7a3b39694241a189c40eb5feaca218c146b095
Andrey <asdukalis@gmail.com>
2020-12-01 23:11:40 +0200

    Added tabs

--- js/script.js
+++ js/script.js
@@ -1,38 +1,36 @@
 'use strict';
-
 window.addEventListener('DOMContentLoaded', () => {
   const tabsParent = document.querySelector('.tabheader__items'),
     tabs = tabsParent.querySelectorAll('.tabheader__item'),
     tabsContent = document.querySelectorAll('.tabcontent');
 
-  function hideTabContent() {
-    tabsContent.forEach((elem) => {
-      elem.classList.add('hide');
-      elem.classList.remove('show', 'fade');
+  function hideTabsContent() {
+    tabsContent.forEach((item) => {
+      item.classList.add('hide');
+      item.classList.remove('show', 'fade');
     });
-
-    tabs.forEach((elem) => {
-      elem.classList.remove('tabheader__item_active');
+    tabs.forEach((tab) => {
+      tab.classList.remove('tabheader__item_active');
     });
   }
 
-  function showTabContent(i = 0) {
+  function showTabsContent(i = 0) {
     tabsContent[i].classList.add('show', 'fade');
     tabsContent[i].classList.remove('hide');
     tabs[i].classList.add('tabheader__item_active');
   }
 
-  hideTabContent();
-  showTabContent();
+  hideTabsContent();
+  showTabsContent();
 
-  tabsParent.addEventListener('cklick', (event) => {
+  tabsParent.addEventListener('click', (event) => {
     const target = event.target;
 
     if (target && target.matches('.tabheader__item')) {
-      tabs.forEach((item, i) => {
-        if (target == item) {
-          hideTabContent();
-          showTabContent(i);
+      tabs.forEach((tab, i) => {
+        if (target == tab) {
+          hideTabsContent();
+          showTabsContent(i);
         }
       });
     }
