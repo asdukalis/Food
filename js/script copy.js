fd5801f8b19a744d119bb85a68141efa1bdccc32
Andrey <asdukalis@gmail.com>
2020-12-04 15:17:26 +0200

    Added timer

--- js/script copy.js
+++ js/script copy.js
@@ -1,38 +1,93 @@
 'use strict';
 window.addEventListener('DOMContentLoaded', () => {
+  //Tabs START
   const tabsParent = document.querySelector('.tabheader__items'),
     tabs = tabsParent.querySelectorAll('.tabheader__item'),
     tabsContent = document.querySelectorAll('.tabcontent');
 
-  function hideTabContent() {
+  function hideTabsContent() {
     tabsContent.forEach((item) => {
       item.classList.add('hide');
       item.classList.remove('show', 'fade');
     });
-    tabs.forEach((item) => {
-      item.classList.remove('tabheader__item_active');
+    tabs.forEach((tab) => {
+      tab.classList.remove('tabheader__item_active');
     });
   }
 
-  function showTabContent(i = 0) {
+  function showTabsContent(i = 0) {
     tabsContent[i].classList.add('show', 'fade');
     tabsContent[i].classList.remove('hide');
     tabs[i].classList.add('tabheader__item_active');
   }
 
-  hideTabContent();
-  showTabContent();
+  hideTabsContent();
+  showTabsContent();
 
   tabsParent.addEventListener('click', (event) => {
     const target = event.target;
 
     if (target && target.matches('.tabheader__item')) {
-      tabs.forEach((item, i) => {
-        if (target == item) {
-          hideTabContent();
-          showTabContent(i);
+      tabs.forEach((tab, i) => {
+        if (target == tab) {
+          hideTabsContent();
+          showTabsContent(i);
         }
       });
     }
   });
+  // tabs END
+
+  // timer START
+  const deadline = '2020-12-31';
+
+  function getTimeRemaining(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date()),
+      days = Math.floor(t / (1000 * 60 * 60 * 24)),
+      hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+      minutes = Math.floor((t / (1000 * 60)) % 60),
+      seconds = Math.floor((t / 1000) % 60);
+
+    return {
+      total: t,
+      days,
+      hours,
+      minutes,
+      seconds,
+    };
+  }
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+      days = timer.querySelector('#days'),
+      hours = timer.querySelector('#hours'),
+      minutes = timer.querySelector('#minutes'),
+      seconds = timer.querySelector('#seconds'),
+      timeInterval = setInterval(updateClock, 1000);
+
+    updateClock(); //  вызываем что-бы не было моргания таймера на странице при перезагрузка страницы,
+    // так как первый вызов этой функции идет с задержкой 1000
+
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+  setClock('.timer', deadline);
+  // timer END
 });
--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 'use strict';
 window.addEventListener('DOMContentLoaded', () => {
+  //Tabs START
   const tabsParent = document.querySelector('.tabheader__items'),
     tabs = tabsParent.querySelectorAll('.tabheader__item'),
     tabsContent = document.querySelectorAll('.tabcontent');
@@ -35,4 +36,56 @@ window.addEventListener('DOMContentLoaded', () => {
       });
     }
   });
+  // timer START
+  const deadline = '2020-12-31';
+
+  function getTimeRemaining(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date()),
+      days = Math.floor(t / (1000 * 60 * 60 * 24)),
+      hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+      minutes = Math.floor((t / (1000 * 60)) % 60),
+      seconds = Math.floor((t / 1000) % 60);
+
+    return {
+      total: t,
+      days,
+      hours,
+      minutes,
+      seconds,
+    };
+  }
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+      days = timer.querySelector('#days'),
+      hours = timer.querySelector('#hours'),
+      minutes = timer.querySelector('#minutes'),
+      seconds = timer.querySelector('#seconds'),
+      timeInterval = setInterval(updateClock, 1000);
+
+    updateClock(); //  вызываем что-бы не было моргания таймера на странице при перезагрузка страницы,
+    // так как первый вызов этой функции идет с задержкой 1000
+
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+  setClock('.timer', deadline);
+  // timer END
 });
